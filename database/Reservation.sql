bad617b0a17e061c0e13a22a0d5eb0077f2484a0
Juhee Min <jhmin128@gmail.com>
2024-09-22 19:20:02 +0900

    fix: fix table

--- database/Reservation.sql
+++ database/Reservation.sql
@@ -1,17 +1,13 @@
-CREATE TABLE "Reservation" (
-                               id BIGSERIAL PRIMARY KEY,
-                               user_id BIGINT NOT NULL,
-                               reservation_date TIMESTAMP NOT NULL,
-                               status VARCHAR(50) NOT NULL,
-                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                               updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                               FOREIGN KEY (user_id) REFERENCES "User"(id) ON DELETE CASCADE
+CREATE TABLE Reservation
+(
+    id               VARCHAR(255) PRIMARY KEY,
+    user_id          VARCHAR(255) NOT NULL,
+    class_id         VARCHAR(255) NOT NULL,
+    reservation_date TIMESTAMP    NOT NULL COMMENT '예약일시',
+    quantity         INT          NOT NULL COMMENT '수량',
+    status           VARCHAR(50)  NOT NULL COMMENT '예약 상태 (예: pending, confirmed, cancelled)',
+    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE,
+    FOREIGN KEY (class_id) REFERENCES Class (id) ON DELETE CASCADE
 );
-
-COMMENT ON TABLE "Reservation" IS '예약 테이블';
-COMMENT ON COLUMN "Reservation".id IS '예약 ID';
-COMMENT ON COLUMN "Reservation".user_id IS '사용자 ID';
-COMMENT ON COLUMN "Reservation".reservation_date IS '예약 날짜 및 시간';
-COMMENT ON COLUMN "Reservation".status IS '예약 상태 (예: pending, confirmed, cancelled)';
-COMMENT ON COLUMN "Reservation".created_at IS '생성일';
-COMMENT ON COLUMN "Reservation".updated_at IS '수정일';
--- database/User.sql
+++ database/User.sql
@@ -1,18 +1,10 @@
-CREATE TABLE "User" (
-                        id BIGSERIAL PRIMARY KEY,
-                        name VARCHAR(255) NOT NULL,
-                        email VARCHAR(255) UNIQUE NOT NULL,
-                        social_provider VARCHAR(50),
-                        social_id VARCHAR(255) UNIQUE,
-                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE User
+(
+    id              VARCHAR(255) PRIMARY KEY COMMENT 'ID',
+    name            VARCHAR(255)        NOT NULL COMMENT '이름',
+    email           VARCHAR(255) UNIQUE NOT NULL COMMENT 'email',
+    social_provider VARCHAR(50) COMMENT '소셜 로그인 제공자',
+    social_id       VARCHAR(255) UNIQUE COMMENT '소셜 로그인 계정 ID',
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
+    updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시'
 );
-
-COMMENT ON TABLE "User" IS '사용자 테이블';
-COMMENT ON COLUMN "User".id IS 'ID';
-COMMENT ON COLUMN "User".name IS '이름';
-COMMENT ON COLUMN "User".email IS 'email';
-COMMENT ON COLUMN "User".social_provider IS '소셜 로그인 제공자';
-COMMENT ON COLUMN "User".social_id IS '소셜 로그인 계정 ID';
-COMMENT ON COLUMN "User".created_at IS '생성일시';
-COMMENT ON COLUMN "User".updated_at IS '수정일시';
